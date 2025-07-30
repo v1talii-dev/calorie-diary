ac86ec346c733b68dba8449db71dbaba88bc569b
Vitalii Demchuk <app.vd@yandex.ru>
2025-07-30 10:57:47 +0300

    Updates

--- assets/index-B_gr_t52.js
+++
@@ -1,1 +0,0 @@
-import{D as r}from"./index-Csu8EE6w.js";import{d as t,g as i,s as o}from"./index-Csu8EE6w.js";export{r as default,t as diaryReducer,i as getFilters,o as setFilters};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/calorie-diary/sw.js', { scope: '/calorie-diary/' })})}
